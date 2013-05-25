7e02af89ba10aa92e261807cc47b0f0879a4a86e
Jason Hamm <jhamm.business@gmail.com>
2013-05-25 06:48:05 -0400

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+  gem "therubyracer"
+  gem "less-rails" #Sprockets (what Rails 3.1 uses for its asset pipeline) supports LESS
+  gem "twitter-bootstrap-rails"
+end
+
+group :development, :test do
+  gem "rspec-rails", ">= 2.0.1"
+  gem 'database_cleaner'
+end
+
+group :test do
+  gem 'cucumber-rails'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JasonhammMe::Application.config.secret_token = '088bda700814ec0c5be1823fc2fd4f8e328363776c2a128b4f5a0071ca071863120c1068b0984ea2830b3f819c4f20b29ce25e3489a12b14443f353109991f75'
